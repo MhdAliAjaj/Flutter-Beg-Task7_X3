0180efac0525b05a987d2e28cc7573e86b229aa4
MhdAliAjaj <engmhdsy630@gmail.com>
2025-11-28 20:10:07 +0300

    add feature home and details screen

---
+++ lib/core/network/apiservice.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:task6/model/productmodel.dart';
+
+
+class ApiService {
+  static const String baseUrl = "https://fakestoreapi.com";
+
+  Future<List<Product>> getProductsByCategoryLimit(String category, int limit) async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/products/category/$category?limit=$limit"),
+    );
+
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data.map((e) => Product.fromJson(e)).toList();
+    } else {
+      throw Exception("Failed to load products");
+    }
+  }
+
+  Future<List<Product>> getProductsByCategory(String category) async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/products/category/$category"),
+    );
+
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data.map((e) => Product.fromJson(e)).toList();
+    } else {
+      throw Exception("Failed to load products");
+    }
+  }
+}
